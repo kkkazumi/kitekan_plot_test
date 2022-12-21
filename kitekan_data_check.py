2ab2feba03a99cda2e8e402d81069382ea88badc
kazumi kumagai <kazumi.kumagai@riken.jp>
2022-12-20 17:55:49 -0800

    add annotation of data number

--- kitekan_data_check.py
+++ kitekan_data_check.py
@@ -2,7 +2,9 @@ import numpy as np
 from scipy import interpolate
 import matplotlib.pyplot as plt
 import pandas as pd
-from func_check_draw import draw_func
+from func_check_draw import draw_func,x_conv
+from adjustText import adjust_text
+
 
 #print("input mode all/each/set(what is it?)/check")
 #mode=input()
@@ -44,8 +46,25 @@ def ret_data(df):
   y_res=np.poly1d(res)(x)
   return res,x_data_2,y_data_2,y_res
 
-def show_graph(username,factor_data,signal_data,factor_type,signal_type,mental_data,thr):
-    x_data = factor_data[:,factor_type]
+def plot_observed_data(x,y,ax):
+  x_sml,x_mid,x_big=x
+  y_sml,y_mid,y_big=y
+  ax.scatter(x_sml,y_sml,color='blue',alpha=0.5,marker="x",label="sml("+str(len(x_sml))+"dots)")
+  ax.scatter(x_mid,y_mid,color='green',alpha=0.5,marker="^",label="mid("+str(len(x_mid))+"dots)")
+  ax.scatter(x_big,y_big,color='red',alpha=0.5,marker="o",label="big("+str(len(x_big))+"dots)")
+
+
+def plot_setting(axs,factor_type,signal_type):
+    for ax in axs:
+        ax.legend()
+        ax.set_ylabel("face data("+face_type_list[signal_type]+")")
+        ax.set_xlabel("factor data("+factor_type_list[factor_type]+")")
+
+
+def show_graph(username,factor_data,signal_data,factor_type,signal_type,mental_data,thr,y_limit=None):
+    _x_data = factor_data[:,factor_type]
+    func_num = 4*factor_type + signal_type
+    x_data = x_conv(_x_data,func_num)
     y_data = signal_data[:,signal_type]
     
     x_max = np.max(x_data)
@@ -65,33 +84,72 @@ def show_graph(username,factor_data,signal_data,factor_type,signal_type,mental_d
     val_big=int(df_big["factor"].count())
 
     if(val_sml*val_mid*val_big>0):
+
       ret_sml,x_sml,y_sml,y_res_sml=ret_data(df_sml)
       ret_mid,x_mid,y_mid,y_res_mid=ret_data(df_mid)
       ret_big,x_big,y_big,y_res_big=ret_data(df_big)
 
+      x_all = x_sml,x_mid,x_big
+      y_all = y_sml,y_mid,y_big
+
+      fig = plt.figure(figsize=(25,12))
+      ax1 = fig.add_subplot(2, 3, 1)
+      ax2 = fig.add_subplot(2, 3, 2)
+      ax3 = ax2.twinx()
+      ax4 = fig.add_subplot(2, 3, 3)
+      ax5 = fig.add_subplot(2, 3, 4)
+      ax6 = fig.add_subplot(2, 3, 6)
+
       x=np.linspace(0,1,100)
 
-      #plt.plot(x,y_res_sml,color='blue',label="sml",linestyle="dotted")
-      #plt.plot(x,y_res_mid,color='green',label="mid",linestyle="dashed")
-      #plt.plot(x,y_res_big,color='red',label="big",linestyle="solid")
-      draw_func(factor_type,signal_type,show_flg=False)
+      xlim=[x_min-(x_max-x_min)*0.1,x_max+(x_max-x_min)*0.1]
+      ylim=[y_min-(y_max-y_min)*0.1,y_max+(y_max-y_min)*0.1]
       
-      plt.scatter(x_sml,y_sml,color='blue',alpha=0.5,marker="x",label="sml("+str(len(x_sml))+"dots)")
-      plt.scatter(x_mid,y_mid,color='green',alpha=0.5,marker="^",label="mid("+str(len(x_mid))+"dots)")
-      plt.scatter(x_big,y_big,color='red',alpha=0.5,marker="o",label="big("+str(len(x_big))+"dots)")
+      plot_observed_data(x_all,y_all,ax1)
+      plot_observed_data(x_all,y_all,ax2)
+      for i in range(len(x_data)):
+          ax1.annotate(str(i), xy = (x_data[i],y_data[i]), size = 8, color = "black")
+      plot_observed_data(x_all,y_all,ax4)
+      plot_observed_data(x_all,y_all,ax5)
+      plot_observed_data(x_all,y_all,ax6)
 
-      plt.xlim(x_min-(x_max-x_min)*0.1,x_max+(x_max-x_min)*0.1)
-      plt.ylim(y_min-(y_max-y_min)*0.1,y_max+(y_max-y_min)*0.1)
+      #plot_observed_data(x_all,y_all,ax5)
+
+      ax5.plot(x_conv(np.linspace(0,1,100),func_num),y_res_sml,color='blue',label="sml")
+      ax5.plot(x_conv(np.linspace(0,1,100),func_num),y_res_mid,color='green',label="mid")
+      ax5.plot(x_conv(np.linspace(0,1,100),func_num),y_res_big,color='red',label="big")
+
+      ax6.plot(x_conv(np.linspace(0,1,100),func_num),y_res_sml,color='blue',label="sml")
+      ax6.plot(x_conv(np.linspace(0,1,100),func_num),y_res_mid,color='green',label="mid")
+      ax6.plot(x_conv(np.linspace(0,1,100),func_num),y_res_big,color='red',label="big")
+
+      draw_func(factor_type,signal_type,ax1,show_flg=False)
+      draw_func(factor_type,signal_type,ax4,show_flg=False)
+
+      if(y_limit is not None):
+        draw_func(factor_type,signal_type,ax3,show_flg=False,x_limit=xlim,y_limit=y_limit)
+        ax3.set_ylim(y_limit)
+      else:
+        draw_func(factor_type,signal_type,ax3,show_flg=False)
+
+      ax1.set_xlim(xlim)
+      ax1.set_ylim(ylim)
+
+      ax2.set_xlim(xlim)
+      ax2.set_ylim(ylim)
+      ax3.set_ylabel("output of basis function("+face_type_list[signal_type]+")")
       
-    plt.legend()
+      ax5.set_xlim(xlim)
+      ax5.set_ylim(ylim)
+
+    plot_setting([ax1,ax2,ax4,ax5,ax6],factor_type,signal_type)
     
+    plt.legend()
     plt.title("mental thr is "+str(thr[1])+" and "+str(thr[2]))
-
-    plt.ylabel("face data("+face_type_list[signal_type]+")")
-    plt.xlabel("factor data("+factor_type_list[factor_type]+")")
 
     filename="./plot/graph_u"+str(username)+"_f"+str(factor_type)+"_s"+str(signal_type)+".png"
     #plt.savefig(filename)
+    #plt.tight_layout()
     plt.show()
     print("u",username,"f",factor_type,"s",signal_type)
     plt.clf()
@@ -111,3 +169,9 @@ if __name__ == '__main__':
 
         factor_data,signal_data,mental_data,df_mental = get_data(username)
         show_graph(username,factor_data,signal_data,f,e,mental_data,thr)
+        #print("input y min")
+        #ymin=float(input())
+        #print("input y max")
+        #ymax=float(input())
+        #ylim=[ymin,ymax]
+        #show_graph(username,factor_data,signal_data,f,e,mental_data,thr,y_limit=ylim)
