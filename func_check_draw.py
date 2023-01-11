fc569766982c78f183a939700f0fca0207f7b98c
kazumi kumagai <kazumi.kumagai@riken.jp>
2023-01-11 00:57:29 -0800

    add quiz_new mode

--- func_check_draw.py
+++ func_check_draw.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 
 
 color_list = ["blue","green","red"]
-label_list = ["small","midd","big"]
+label_list = ["low mood","medium mood","high mood"]
+linestyle_list = ["dashdot","dashed","solid"]
+
+quiz_new_val = [80,80,80,80,
+        1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,
+        1,1,1,1,1,1,1,1,
+        240,240,240,240,
+        1,1,1,1,10,10,10,10,
+        10,10,10,10,10,10,10,10,
+        10,10,10,10,10,10,10,10,
+        5,5,5,5]
 
 quiz_norm_val = [80,80,80,80,
         1,1,1,1,1,1,1,1,
@@ -12,12 +23,15 @@ quiz_norm_val = [80,80,80,80,
         240,240,240,240,
         80,80,80,80,
         80,80,80,80]
+
 #TODO define bono_norm_val
 bono_norm_val = [1,1,1,1,
         1,1,1,1]
 
 def get_data(mode):
     if(mode=="quiz"):
+        data = np.loadtxt("func_check.csv",delimiter=",")
+    elif(mode=="quiz_new"):
         data = np.loadtxt("func_check.csv",delimiter=",")
     elif(mode=="bono"):
         data = np.loadtxt("func_check_bono.csv",delimiter=",")
@@ -29,6 +43,11 @@ def x_conv(mode,x,func_num):
             conv_x = x * quiz_norm_val[func_num] - 80.0
         else:
             conv_x = x * quiz_norm_val[func_num]
+    elif(mode=="quiz_new"):
+        if((func_num > 27) and (func_num<32)):
+            conv_x = x * quiz_new_val[func_num] - 80.0
+        else:
+            conv_x = x * quiz_new_val[func_num]
     elif(mode=="bono"):
         #conv_x = x * bono_norm_val[func_num]
         conv_x = x
@@ -54,7 +73,7 @@ def draw_func(mode,f,e,ax,show_flg=True,x_limit=None,y_limit=None):
         col_num = 12*f+3*e+m
         print(col_num)
         col_data = data[col_num,:]
-        ax.plot(x,col_data,color=color_list[m],label=label_list[m])
+        ax.plot(x,col_data,color=color_list[m],label=label_list[m],linestyle=linestyle_list[m])
         if(y_limit is not None):
             x_max=xlim[1]
             x_min=xlim[0]
